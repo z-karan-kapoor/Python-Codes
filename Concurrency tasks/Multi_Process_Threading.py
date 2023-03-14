15cbc3bfa718e79335d00534f1619415470d85e4
z-karan-kapoor <124143199+z-karan-kapoor@users.noreply.github.com>
2023-03-14 17:42:39 +0530

    OS_Session_14

--- Concurrency tasks/Multi_Process_Threading.py
+++ Concurrency tasks/Multi_Process_Threading.py
@@ -2,19 +2,19 @@ from multiprocessing import Process
 
 
 class mProcess(object):  # Class for MultiProcessing
-    def __init__(self, func, idxv, procs):  # Constructor
+    def __init__(self, func, args, procs):  # Constructor
         self.func = func
-        self.idxv = idxv
+        self.args = args
         self.procs = procs
 
     def run(self):  # Run Processes
         procs = []
 
         # Divide Tasks over number of Processes
-        tmp = int(len(self.idxv)/self.procs)
-        x = [self.idxv[i:i+tmp] for i in range(0, len(self.idxv), tmp)]
+        tmp = int(len(self.args)/self.procs)
+        tmplist = [self.args[i:i+tmp] for i in range(0, len(self.args), tmp)]
 
-        for i in x:
+        for i in tmplist:
             proc = Process(target=self.func, args=(i,))  # Allocate Process
             procs.append(proc)
             proc.start()  # Starts Process
@@ -26,15 +26,11 @@ class mProcess(object):  # Class for MultiProcessing
 def fun(nums=[]):  # Function to square List elements
     ls = []
     [ls.append(i ** 2) for i in nums]
-    if (ls != []):
-        print("[", end="")
-        for i in ls:
-            print(f" {i} ", end="")
-        print("]")
+    print (ls)
 
 
 if __name__ == '__main__':  # Main function
     nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    pno = 3
-    obj = mProcess(fun, nums, pno)
+    procs = 3
+    obj = mProcess(fun, nums, procs)
     obj.run()
